ac5db45e21b2f66a98a9b8ed580046a2bfc6c775
Marcelovmendes <marcelovmendescontato@gmail.com>
2025-03-15 17:47:43 -0300

    feat: add spotifyApi configuration

--- spotify-service/src/test/java/com/example/spotify/SpotifyServiceApplicationTests.java
+++ spotify-service/src/test/java/com/example/spotify/SpotifyServiceApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.spotify;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = {"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"})
 class SpotifyServiceApplicationTests {
 
 	@Test
