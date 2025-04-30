063d8f3e7008627b2f4b1aca901e9a2cead20068
Marcelovmendes <marcelovmendescontato@gmail.com>
2025-04-30 10:30:05 -0300

    refactor: implement early return on SpotifyExceptionTranslator

--- spotify-service/src/main/java/com/example/spotify/common/exception/SpotifyApiExceptionTranslator.java
+++ spotify-service/src/main/java/com/example/spotify/common/exception/SpotifyApiExceptionTranslator.java
@@ -11,18 +11,30 @@ public class SpotifyApiExceptionTranslator {
 
     public RuntimeException translate (Exception e) {
 
+        if (e instanceof ApplicationException) return (ApplicationException) e;
+
         if (e instanceof IOException) {
-            return new InfrastructureException("Error communicating with Spotify API", e, ErrorType.SPOTIFY_API_EXCEPTION);
-        } else if (e instanceof SpotifyWebApiException) {
-            if (e.getMessage().contains("401")) {
-                return new AuthenticationException("Token is invalid or expired", e, ErrorType.TOKEN_EXPIRED);
-            }
-            return new SpotifyApiException("Spotify API error", e, ErrorType.SPOTIFY_API_EXCEPTION);
-        } else if (e instanceof ParseException) {
-            return new InfrastructureException("Analysis error while parsing response", e, ErrorType.SPOTIFY_API_EXCEPTION);
-        } else if (e instanceof ApplicationException) {
-            return (ApplicationException) e;
+            return new InfrastructureException(
+                    "Error communicating with Spotify API", e, ErrorType.SPOTIFY_API_EXCEPTION
+            );
         }
-        return new InfrastructureException("Unexpected error while communicating with Spotify API", e, ErrorType.GENERAL_EXCEPTION);
+        if (e instanceof SpotifyWebApiException) {
+            if (e.getMessage().contains("401")) {
+                return new AuthenticationException(
+                        "Token is invalid or expired", e, ErrorType.TOKEN_EXPIRED
+                );
+            }
+            return new SpotifyApiException(
+                    "Spotify API error", e, ErrorType.SPOTIFY_API_EXCEPTION
+            );
+        }
+        if (e instanceof ParseException) {
+            return new InfrastructureException(
+                    "Analysis error while parsing response", e, ErrorType.SPOTIFY_API_EXCEPTION
+            );
+        }
+        return new InfrastructureException(
+                "Unexpected error while communicating with Spotify API", e, ErrorType.GENERAL_EXCEPTION
+        );
     }
 }
