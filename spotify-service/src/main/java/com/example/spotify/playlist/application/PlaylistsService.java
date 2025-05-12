0f282c90635ba4beec507177fae5f94a03a9e673
Marcelovmendes <marcelovmendescontato@gmail.com>
2025-05-12 19:37:32 -0300

    refactor: remove imports

--- spotify-service/src/main/java/com/example/spotify/playlist/application/PlaylistsService.java
+++ spotify-service/src/main/java/com/example/spotify/playlist/application/PlaylistsService.java
@@ -2,9 +2,6 @@ package com.example.spotify.playlist.application;
 
 import com.example.spotify.playlist.domain.entity.Playlist;
 import com.example.spotify.playlist.domain.entity.Track;
-import se.michaelthelin.spotify.model_objects.specification.Paging;
-import se.michaelthelin.spotify.model_objects.specification.PlaylistSimplified;
-import se.michaelthelin.spotify.model_objects.specification.PlaylistTrack;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
